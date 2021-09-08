91ec6c8be5be6cca12bd505bee1b3f6c9fff4038
xxulverxx <35438607+rjrm84@users.noreply.github.com>
2021-09-08 17:52:24 -0300

    adding one file to test

---
+++ __test__/uiChecker.test.js
@@ -0,0 +1,7 @@
+import uiChecker  from '../src/client/js/uiChecker'
+
+describe("Testing the URl validation  functionality", () => {
+    test("Testing the uiChecker() function", () => {
+            expect(uiChecker).toBeDefined();
+        }
+    )})
