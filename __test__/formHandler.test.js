e81b823e30f14cd3c41a3c672f5985f9075d5cf6
xxulverxx <35438607+rjrm84@users.noreply.github.com>
2021-09-08 16:38:44 -0300

    Ready for Production

---
+++ __test__/formHandler.test.js
@@ -0,0 +1,7 @@
+import  urlValidation  from '../src/client/js/formHandler'
+
+describe("Testing the UI update functionality", () => {
+    test("Testing the urlValidation() function", () => {
+            expect(urlValidation).toBeDefined();
+        }
+    )})
---
+++ __test__/nameChecker.test.js
@@ -0,0 +1,8 @@
+// Import the js file to test
+import urlValidation from "../src/client/js/nameChecker";
+// The describe() function
+describe("Testing the URL validity", () => {
+    // The test() function
+    test("Testing the urlValidation() function", () => {
+           expect(urlValidation).toBeDefined();
+})});
